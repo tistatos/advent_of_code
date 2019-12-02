14600e686e7b87405d5faf565e4e92ab48f218e9
Erik Sandrén <erik.sandren@dataton.se>
2019-12-02 01:38:01 +0100

    day 1 part 2 done

--- src/day1.rs
+++ src/day1.rs
@@ -5,8 +5,22 @@ use self::aoc::get_rows;
 
 fn main() {
     let input = get_rows("input_data/day_1");
+    let fuel_function = |x: i32| x / 3 - 2;
 
-    let sum: i32 = input.iter().map(|x| x / 3 - 2).sum();
+    let sum: i32 = input.iter().map(|x| fuel_function(*x)).sum();
 
-    println!("sum is {}", sum);
+    println!("part 1 sum is {}", sum);
+
+
+    let sum = input.iter().fold(0, |mut acc, x| {
+        let mut fuel = *x;
+        while fuel > 0 {
+            fuel = fuel_function(fuel);
+            if fuel > 0 {
+                acc += fuel;
+            }
+        }
+        acc
+    });
+    println!("part 2 sum is {}", sum);
 }
