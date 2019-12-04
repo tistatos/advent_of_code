1a50399e243daf85fd68c403cf0c1602eda6ba1d
Erik Sandrén <erik.sandren@dataton.se>
2019-12-04 11:55:54 +0100

    day 4 use threads

--- src/day4.rs
+++ src/day4.rs
@@ -1,21 +1,15 @@
 mod aoc;
 use std::env;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::thread;
 
 fn get_digit(number: u32, position: u32) -> u32 {
     number / 10u32.pow(position - 1) % 10
 }
 
-pub fn main() {
-
-    let mut args = env::args().skip(1);
-    let start: u32 = args.next().unwrap().parse::<u32>().unwrap();
-    let stop: u32 = args.next().unwrap().parse::<u32>().unwrap();
-
-    let mut valid_codes_part_1 = 0;
-    let mut valid_codes_part_2 = 0;
-
+pub fn find_codes(start: u32, stop: u32, counter_part_1: Arc<AtomicUsize>, counter_part_2: Arc<AtomicUsize>) {
     for number in start..stop {
-
         let mut found_adjacent = false;
         let mut found_pair = false;
         let mut adjacent_count = 1;
@@ -45,13 +39,43 @@ pub fn main() {
         }
 
         if valid && found_adjacent {
-            valid_codes_part_1 += 1;
+            counter_part_1.fetch_add(1, Ordering::SeqCst);
         }
 
         if valid && found_pair {
-            valid_codes_part_2 += 1;
+            counter_part_2.fetch_add(1, Ordering::SeqCst);
         }
     }
-    println!("number {} valid codes part 1", valid_codes_part_1);
-    println!("number {} valid codes part 2", valid_codes_part_2);
+}
+
+
+pub fn main() {
+
+    let mut args = env::args().skip(1);
+    let start: u32 = args.next().unwrap().parse::<u32>().unwrap();
+    let stop: u32 = args.next().unwrap().parse::<u32>().unwrap();
+
+    let range = stop - start;
+    const THREADS: u32 = 4;
+    let thread_range = range / THREADS;
+
+    let mut valid_codes_part_1 = Arc::new(AtomicUsize::new(0));
+    let mut valid_codes_part_2 = Arc::new(AtomicUsize::new(0));
+
+    let mut handles = vec![];
+    for i in 0..THREADS {
+        let p1 = valid_codes_part_1.clone();
+        let p2 = valid_codes_part_2.clone();
+
+        handles.push(
+            thread::spawn(move||
+                find_codes(start + i * thread_range, start + (i + 1) * thread_range, p1, p2))
+        );
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("number {} valid codes part 1", valid_codes_part_1.load(Ordering::SeqCst));
+    println!("number {} valid codes part 2", valid_codes_part_2.load(Ordering::SeqCst));
 }
