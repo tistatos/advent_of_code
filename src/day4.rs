2864c5c716b4aaf771f9ba197a705d5428fc2f9e
Erik Sandrén <erik.sandren@dataton.se>
2019-12-04 11:31:11 +0100

    day 4 part 2 done

--- src/day4.rs
+++ src/day4.rs
@@ -12,10 +12,16 @@ pub fn main() {
     let stop: u32 = args.next().unwrap().parse::<u32>().unwrap();
 
     let mut valid_codes_part_1 = 0;
+    let mut valid_codes_part_2 = 0;
+
     for number in start..stop {
-        let mut previous_digit = get_digit(number, 1);
+
         let mut found_adjacent = false;
+        let mut found_pair = false;
+        let mut adjacent_count = 1;
         let mut valid = true;
+
+        let mut previous_digit = get_digit(number, 1);
         for i in 2..7 {
             let current_digit = get_digit(number, i);
             if current_digit > previous_digit {
@@ -24,13 +30,28 @@ pub fn main() {
             }
             else if current_digit == previous_digit {
                 found_adjacent = true;
+                adjacent_count += 1;
+            }
+            else {
+                if adjacent_count == 2 {
+                    found_pair = true;
+                }
+                adjacent_count = 1;
             }
             previous_digit = current_digit
+        }
+        if adjacent_count == 2 {
+            found_pair = true;
         }
 
         if valid && found_adjacent {
             valid_codes_part_1 += 1;
         }
+
+        if valid && found_pair {
+            valid_codes_part_2 += 1;
+        }
     }
     println!("number {} valid codes part 1", valid_codes_part_1);
+    println!("number {} valid codes part 2", valid_codes_part_2);
 }
