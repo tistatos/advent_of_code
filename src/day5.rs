33e763022b47fd9bd806737abe0d501c693dcc49
Erik Sandrén <erik.sandren@dataton.se>
2019-12-05 22:39:24 +0100

    day 5 part 2 done

--- src/day5.rs
+++ src/day5.rs
@@ -2,8 +2,8 @@ mod aoc;
 use self::aoc::get_csv;
 
 enum Mode {
-    Position = 0,
-    Immediate = 1
+    Position,
+    Immediate
 }
 impl std::convert::From<i32> for Mode {
     fn from(val: i32) -> Mode {
@@ -21,11 +21,15 @@ fn get_digit(number: i32, position: u32) -> i32 {
 
 #[derive(PartialEq, Debug)]
 enum Op {
-    Add = 1,
-    Mul = 2,
-    Input = 3,
-    Output = 4,
-    Halt = 99,
+    Add,
+    Mul,
+    Input,
+    Output,
+    JNZ,
+    JZ,
+    LE,
+    EQ,
+    Halt,
 }
 impl std::convert::From<i32> for Op {
     fn from(val: i32) -> Op {
@@ -34,6 +38,10 @@ impl std::convert::From<i32> for Op {
             2 => Op::Mul, //3 parameters
             3 => Op::Input, //1 parameter
             4 => Op::Output, //1 parameter
+            5 => Op::JNZ, //2 parameter
+            6 => Op::JZ, //2 parameter
+            7 => Op::LE, //3 parameter
+            8 => Op::EQ, //3 parameter
             99 => Op::Halt, //0 parameters
             err => panic!("unrecognized opcode: {}", err)
         }
@@ -45,7 +53,8 @@ fn parse_op(code: i32) -> (Op, Option<Mode>, Option<Mode>) {
         Op::from(
             get_digit(code, 2) * 10 + get_digit(code, 1));
     let (lhs, rhs) = match op {
-        Op::Add | Op::Mul => {
+        Op::Add | Op::Mul | Op::LE | Op::EQ
+            | Op::JNZ | Op::JZ => {
             (Some(Mode::from(get_digit(code,3))),
             Some(Mode::from(get_digit(code,4))))
         }
@@ -104,9 +113,70 @@ fn execute_program(mut program: Vec<i32>, input: i32) -> Vec<i32> {
                 i += 2;
             },
             Op::Output => {
-                let src = program[i+1]; //always position?
-                output = program[src as usize];
+                let src = match lhs_mode.unwrap() {
+                    Mode::Position => program[program[i+1] as usize],
+                    Mode::Immediate => program[i+1]
+                };
+                output = src;
                 i += 2;
+            },
+            Op::JNZ => {
+                let operand = match lhs_mode.unwrap() {
+                    Mode::Position => program[program[i+1] as usize],
+                    Mode::Immediate => program[i+1]
+                };
+                let dest = match rhs_mode.unwrap() {
+                    Mode::Position => program[program[i+2] as usize],
+                    Mode::Immediate => program[i+2]
+                };
+                if operand != 0 {
+                    i = dest as usize;
+                }
+                else {
+                    i += 3;
+                }
+            },
+            Op::JZ => {
+                let operand = match lhs_mode.unwrap() {
+                    Mode::Position => program[program[i+1] as usize],
+                    Mode::Immediate => program[i+1]
+                };
+                let dest = match rhs_mode.unwrap() {
+                    Mode::Position => program[program[i+2] as usize],
+                    Mode::Immediate => program[i+2]
+                };
+                if operand == 0 {
+                    i = dest as usize;
+                }
+                else {
+                    i += 3;
+                }
+            },
+            Op::LE => {
+                let lhs = match lhs_mode.unwrap() {
+                    Mode::Position => program[program[i+1] as usize],
+                    Mode::Immediate => program[i+1]
+                };
+                let rhs = match rhs_mode.unwrap() {
+                    Mode::Position => program[program[i+2] as usize],
+                    Mode::Immediate => program[i+2]
+                };
+                let dest = program[i+3]; //always position?
+                program[dest as usize] = (lhs < rhs) as i32;
+                i += 4;
+            },
+            Op::EQ => {
+                let lhs = match lhs_mode.unwrap() {
+                    Mode::Position => program[program[i+1] as usize],
+                    Mode::Immediate => program[i+1]
+                };
+                let rhs = match rhs_mode.unwrap() {
+                    Mode::Position => program[program[i+2] as usize],
+                    Mode::Immediate => program[i+2]
+                };
+                let dest = program[i+3]; //always position?
+                program[dest as usize] = (lhs == rhs) as i32;
+                i += 4;
             },
             _ => panic!("Unexpected instruction")
         }
