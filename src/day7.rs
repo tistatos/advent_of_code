88ab0b6095788644aed55fb03dae7fe2d120a5b9
Erik Sandrén <erik.sandren@dataton.se>
2019-12-10 12:04:49 +0100

    day 7 part 2 done

--- src/day7.rs
+++ src/day7.rs
@@ -6,6 +6,7 @@ enum Mode {
     Position,
     Immediate
 }
+
 impl std::convert::From<i32> for Mode {
     fn from(val: i32) -> Mode {
         match val {
@@ -69,127 +70,138 @@ fn parse_op(code: i32) -> (Op, Option<Mode>, Option<Mode>) {
     (op, lhs, rhs)
 }
 
-fn execute_program(mut program: Vec<i32>, mut input: Vec<i32>) -> i32 {
-    let mut i = 0;
-    let mut output = 0;
-    loop {
-        let (op, lhs_mode, rhs_mode) = parse_op(program[i]);
+struct Amplifier {
+    ip: usize,
+    program: Vec<i32>,
+    halted: bool
+}
 
-        if op == Op::Halt {
-            break;
+impl Amplifier {
+    fn execute_program(&mut self, mut input: Vec<i32>) -> i32 {
+        let mut output = 0;
+        loop {
+            let (op, lhs_mode, rhs_mode) = parse_op(self.program[self.ip]);
+
+            if op == Op::Halt {
+                self.halted = true;
+                break;
+            }
+
+            match op {
+                Op::Add => {
+                    let lhs = match lhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+1] as usize],
+                        Mode::Immediate => self.program[self.ip+1]
+                    };
+                    let rhs = match rhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+2] as usize],
+                        Mode::Immediate => self.program[self.ip+2]
+                    };
+
+                    let dest = self.program[self.ip+3]; //always position?
+                    self.program[dest as usize] = lhs + rhs;
+                    self.ip += 4;
+                },
+                Op::Mul => {
+                    let lhs = match lhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+1] as usize],
+                        Mode::Immediate => self.program[self.ip+1]
+                    };
+                    let rhs = match rhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+2] as usize],
+                        Mode::Immediate => self.program[self.ip+2]
+                    };
+
+                    let dest = self.program[self.ip+3]; //always position?
+                    self.program[dest as usize] = lhs * rhs;
+                    self.ip += 4;
+                },
+                Op::Input => {
+                    let dest = self.program[self.ip+1]; //always position?
+                    let inp = input.pop().unwrap();
+                    self.program[dest as usize] = inp;
+                    self.ip += 2;
+                },
+                Op::Output => {
+                    let src = match lhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+1] as usize],
+                        Mode::Immediate => self.program[self.ip+1]
+                    };
+                    output = src;
+                    self.ip += 2;
+                    break;
+                },
+                Op::JNZ => {
+                    let operand = match lhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+1] as usize],
+                        Mode::Immediate => self.program[self.ip+1]
+                    };
+                    let dest = match rhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+2] as usize],
+                        Mode::Immediate => self.program[self.ip+2]
+                    };
+                    if operand != 0 {
+                        self.ip = dest as usize;
+                    }
+                    else {
+                        self.ip += 3;
+                    }
+                },
+                Op::JZ => {
+                    let operand = match lhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+1] as usize],
+                        Mode::Immediate => self.program[self.ip+1]
+                    };
+                    let dest = match rhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+2] as usize],
+                        Mode::Immediate => self.program[self.ip+2]
+                    };
+                    if operand == 0 {
+                        self.ip = dest as usize;
+                    }
+                    else {
+                        self.ip += 3;
+                    }
+                },
+                Op::LE => {
+                    let lhs = match lhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+1] as usize],
+                        Mode::Immediate => self.program[self.ip+1]
+                    };
+                    let rhs = match rhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+2] as usize],
+                        Mode::Immediate => self.program[self.ip+2]
+                    };
+                    let dest = self.program[self.ip+3]; //always position?
+                    self.program[dest as usize] = (lhs < rhs) as i32;
+                    self.ip += 4;
+                },
+                Op::EQ => {
+                    let lhs = match lhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+1] as usize],
+                        Mode::Immediate => self.program[self.ip+1]
+                    };
+                    let rhs = match rhs_mode.unwrap() {
+                        Mode::Position => self.program[self.program[self.ip+2] as usize],
+                        Mode::Immediate => self.program[self.ip+2]
+                    };
+                    let dest = self.program[self.ip+3]; //always position?
+                    self.program[dest as usize] = (lhs == rhs) as i32;
+                    self.ip += 4;
+                },
+                _ => panic!("Unexpected instruction")
+            }
         }
-
-        match op {
-            Op::Add => {
-                let lhs = match lhs_mode.unwrap() {
-                    Mode::Position => program[program[i+1] as usize],
-                    Mode::Immediate => program[i+1]
-                };
-                let rhs = match rhs_mode.unwrap() {
-                    Mode::Position => program[program[i+2] as usize],
-                    Mode::Immediate => program[i+2]
-                };
-
-                let dest = program[i+3]; //always position?
-                program[dest as usize] = lhs + rhs;
-                i += 4;
-            },
-            Op::Mul => {
-                let lhs = match lhs_mode.unwrap() {
-                    Mode::Position => program[program[i+1] as usize],
-                    Mode::Immediate => program[i+1]
-                };
-                let rhs = match rhs_mode.unwrap() {
-                    Mode::Position => program[program[i+2] as usize],
-                    Mode::Immediate => program[i+2]
-                };
-
-                let dest = program[i+3]; //always position?
-                program[dest as usize] = lhs * rhs;
-                i += 4;
-            },
-            Op::Input => {
-                let dest = program[i+1]; //always position?
-                let inp = input.pop().unwrap();
-                program[dest as usize] = inp;
-                i += 2;
-            },
-            Op::Output => {
-                let src = match lhs_mode.unwrap() {
-                    Mode::Position => program[program[i+1] as usize],
-                    Mode::Immediate => program[i+1]
-                };
-                output = src;
-                i += 2;
-            },
-            Op::JNZ => {
-                let operand = match lhs_mode.unwrap() {
-                    Mode::Position => program[program[i+1] as usize],
-                    Mode::Immediate => program[i+1]
-                };
-                let dest = match rhs_mode.unwrap() {
-                    Mode::Position => program[program[i+2] as usize],
-                    Mode::Immediate => program[i+2]
-                };
-                if operand != 0 {
-                    i = dest as usize;
-                }
-                else {
-                    i += 3;
-                }
-            },
-            Op::JZ => {
-                let operand = match lhs_mode.unwrap() {
-                    Mode::Position => program[program[i+1] as usize],
-                    Mode::Immediate => program[i+1]
-                };
-                let dest = match rhs_mode.unwrap() {
-                    Mode::Position => program[program[i+2] as usize],
-                    Mode::Immediate => program[i+2]
-                };
-                if operand == 0 {
-                    i = dest as usize;
-                }
-                else {
-                    i += 3;
-                }
-            },
-            Op::LE => {
-                let lhs = match lhs_mode.unwrap() {
-                    Mode::Position => program[program[i+1] as usize],
-                    Mode::Immediate => program[i+1]
-                };
-                let rhs = match rhs_mode.unwrap() {
-                    Mode::Position => program[program[i+2] as usize],
-                    Mode::Immediate => program[i+2]
-                };
-                let dest = program[i+3]; //always position?
-                program[dest as usize] = (lhs < rhs) as i32;
-                i += 4;
-            },
-            Op::EQ => {
-                let lhs = match lhs_mode.unwrap() {
-                    Mode::Position => program[program[i+1] as usize],
-                    Mode::Immediate => program[i+1]
-                };
-                let rhs = match rhs_mode.unwrap() {
-                    Mode::Position => program[program[i+2] as usize],
-                    Mode::Immediate => program[i+2]
-                };
-                let dest = program[i+3]; //always position?
-                program[dest as usize] = (lhs == rhs) as i32;
-                i += 4;
-            },
-            _ => panic!("Unexpected instruction")
-        }
+        output
     }
-
-    output
+    fn will_halt(&self) -> bool {
+        let (op, _, _) = parse_op(self.program[self.ip]);
+        op == Op::Halt
+    }
 }
 
 fn main() {
     let mut largest_result = 0;
-    //let program = vec!(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0);
     let program = get_csv("input_data/day_7", ",");
     for a in 0..5 {
         for b in 0..5 {
@@ -206,11 +218,60 @@ fn main() {
                             continue;
                         }
 
-                        let result = execute_program(program.clone(), vec!(0, a)); //A
-                        let result = execute_program(program.clone(), vec!(result, b)); //B
-                        let result = execute_program(program.clone(), vec!(result, c)); //C
-                        let result = execute_program(program.clone(), vec!(result, d)); //D
-                        let result = execute_program(program.clone(), vec!(result, e)); //E
+                        let mut a_amp = Amplifier{ ip: 0, program: program.clone(), halted: false};
+                        let mut b_amp = Amplifier{ ip: 0, program: program.clone(), halted: false};
+                        let mut c_amp = Amplifier{ ip: 0, program: program.clone(), halted: false};
+                        let mut d_amp = Amplifier{ ip: 0, program: program.clone(), halted: false};
+                        let mut e_amp = Amplifier{ ip: 0, program: program.clone(), halted: false};
+
+                        let result = a_amp.execute_program(vec!(0, a)); //A
+                        let result = b_amp.execute_program(vec!(result, b)); //B
+                        let result = c_amp.execute_program(vec!(result, c)); //C
+                        let result = d_amp.execute_program(vec!(result, d)); //D
+                        let result = e_amp.execute_program(vec!(result, e)); //E
+                        if result > largest_result {
+                            largest_result = result;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("day 7 part 1: {}", largest_result );
+
+    for a in 5..10 {
+        for b in 5..10 {
+            for c in 5..10 {
+                for d in 5..10 {
+                    for e in 5..10 {
+                        let mut all_unique = HashSet::new();
+                        all_unique.insert(a);
+                        all_unique.insert(b);
+                        all_unique.insert(c);
+                        all_unique.insert(d);
+                        all_unique.insert(e);
+                        if all_unique.len() != 5 {
+                            continue;
+                        }
+
+                        let mut a_amp = Amplifier{ ip: 0, program: program.clone(), halted: false};
+                        let mut b_amp = Amplifier{ ip: 0, program: program.clone(), halted: false};
+                        let mut c_amp = Amplifier{ ip: 0, program: program.clone(), halted: false};
+                        let mut d_amp = Amplifier{ ip: 0, program: program.clone(), halted: false};
+                        let mut e_amp = Amplifier{ ip: 0, program: program.clone(), halted: false};
+                        let mut result = a_amp.execute_program(vec!(0, a)); //A
+                        result = b_amp.execute_program(vec!(result, b)); //B
+                        result = c_amp.execute_program(vec!(result, c)); //C
+                        result = d_amp.execute_program(vec!(result, d)); //D
+                        result = e_amp.execute_program(vec!(result, e)); //E
+                        while !a_amp.will_halt() {
+                            result = a_amp.execute_program(vec!(result)); //A
+                            result = b_amp.execute_program(vec!(result)); //B
+                            result = c_amp.execute_program(vec!(result)); //C
+                            result = d_amp.execute_program(vec!(result)); //D
+                            result = e_amp.execute_program(vec!(result)); //E
+                        }
+
                         if result > largest_result {
                             largest_result = result;
                         }
@@ -220,5 +281,5 @@ fn main() {
         }
     }
 
-    println!("day 7 part 1: {}", largest_result );
+    println!("day 7 part 2: {}", largest_result );
 }
