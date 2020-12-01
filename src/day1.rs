85def7ef1a5361b80c6170b066168c2015967b35
tistatos <tistatos@gmail.com>
2020-12-01 11:06:02 +0100

    Day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -5,15 +5,31 @@ use self::aoc::get_rows;
 fn main() {
     let input: Vec<u32> = get_rows("input_data/day_1");
 
-    for i in &input {
-        if *i > 2020 {
+    let mut part_one = 0;
+    let mut part_two = 0;
+
+    for i in 0..input.len() {
+        let a = input[i];
+        if a > 2020 {
             continue;
         }
-        for j in &input {
-            if *i + *j == 2020 {
-                println!("Solution day 1: {}", *i * *j);
-                break;
+        for j in (i + 1)..input.len() {
+            let b = input[j];
+            if a + b == 2020 {
+                part_one = a * b;
+            }
+            for k in (j + 1)..input.len() {
+                if a + b > 2020 {
+                    continue;
+                }
+                let c = input[k];
+                if a + b + c == 2020 {
+                    part_two = a * b * c;
+                }
             }
         }
     }
+
+    println!("Solution day 1 part 1: {}", part_one);
+    println!("Solution day 1 part 2: {}", part_two);
 }
