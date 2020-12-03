6ddc26e500c080e6d1f3c87f08744f77c986cad6
tistatos <tistatos@gmail.com>
2020-12-03 10:18:59 +0100

    Day 3 improved

--- src/day3.rs
+++ src/day3.rs
@@ -6,25 +6,21 @@ static TREE: char = '#';
 pub fn main() {
     let input = get_string_rows("input_data/day_3");
     let patterns = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-    let steps = input.len();
-    let width = input[0].len();
 
-    let mut tree_count = vec![];
-    for p in patterns {
-        let mut p_count: u64 = 0;
-        let mut loop_c = 0;
-        for i in (0..steps).step_by(p.1) {
-            let ref row = input[i];
-            let index = (loop_c * p.0) % width;
-            loop_c += 1;
-            let spot = row.chars().nth(index).unwrap();
-            if spot == TREE {
-                p_count += 1;
-            }
-        }
-        println!("p: {:?} {}", p, p_count);
-        tree_count.push(p_count)
-    }
+    let tree_count: Vec<u64> = patterns
+        .iter()
+        .map(|p| {
+            input
+                .iter()
+                .step_by(p.1)
+                .enumerate()
+                .map(|(i, r)| {
+                    let index = (i * p.0) % r.len();
+                    (r.chars().nth(index).unwrap() == TREE) as u64
+                })
+                .sum()
+        })
+        .collect();
     println!("day 3 part 1: {}", tree_count[1]);
     println!(
         "day 3 part 2: {}",
