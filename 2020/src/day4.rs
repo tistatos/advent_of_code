1d5e60123c46b1308e960f400c0a39caa39d2a07
tistatos <tistatos@gmail.com>
2020-12-04 11:08:50 +0100

    day 4 part 2 done

--- 2020/src/day4.rs
+++ 2020/src/day4.rs
@@ -9,7 +9,98 @@ fn valid_passport(passport: &String) -> bool {
         && passport.contains("hcl")
         && passport.contains("ecl")
         && passport.contains("pid")
-    //passport.contains("cid")
+}
+
+fn valid_passport_entries(passport: &String) -> bool {
+    if passport.contains("byr")
+        && passport.contains("iyr")
+        && passport.contains("eyr")
+        && passport.contains("hgt")
+        && passport.contains("hcl")
+        && passport.contains("ecl")
+        && passport.contains("pid")
+    {
+        for entry in passport.split(" ") {
+            let e: Vec<&str> = entry.split(":").collect();
+            match e[0] {
+                "byr" => {
+                    let val = e[1].parse::<u32>().unwrap();
+                    if val < 1920 || val > 2002 {
+                        return false;
+                    }
+                }
+                "iyr" => {
+                    let val = e[1].parse::<u32>().unwrap();
+                    if val < 2010 || val > 2020 {
+                        return false;
+                    }
+                }
+                "eyr" => {
+                    let val = e[1].parse::<u32>().unwrap();
+                    if val < 2020 || val > 2030 {
+                        return false;
+                    }
+                }
+                "hgt" => {
+                    let val = e[1];
+                    if val.contains("cm") {
+                        let index = val.find("cm").unwrap();
+                        let h = e[1][0..index].parse::<u32>().unwrap();
+                        if h < 150 || h > 193 {
+                            return false;
+                        }
+                    } else if val.contains("in") {
+                        let index = val.find("in").unwrap();
+                        let h = e[1][0..index].parse::<u32>().unwrap();
+                        if h < 59 || h > 76 {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                "hcl" => {
+                    let val = e[1];
+                    if val.contains("#") {
+                        let count = val
+                            .chars()
+                            .filter(|&c| "1234567890abcdef".contains(c))
+                            .count();
+                        if count != 6 {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                "ecl" => {
+                    let val = e[1];
+                    if val.contains("amb")
+                        || val.contains("blu")
+                        || val.contains("brn")
+                        || val.contains("gry")
+                        || val.contains("grn")
+                        || val.contains("hzl")
+                        || val.contains("oth")
+                    {
+                    } else {
+                        return false;
+                    }
+                }
+                "pid" => {
+                    let val = e[1];
+                    let count = val.chars().filter(|&c| "1234567890".contains(c)).count();
+                    if count != 9 {
+                        return false;
+                    }
+                }
+                _ => {}
+            }
+        }
+        true
+    } else {
+        false
+    }
 }
 
 pub fn main() {
@@ -28,5 +119,10 @@ pub fn main() {
     passports.push(passport);
 
     let valid = passports.iter().filter(|p| valid_passport(&p)).count();
+    let valid_entries = passports
+        .iter()
+        .filter(|p| valid_passport_entries(&p))
+        .count();
     println!("day 4 part 1: {}", valid);
+    println!("day 4 part 1: {}", valid_entries);
 }
