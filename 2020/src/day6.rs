f1d0725e1659dde6d277827546b703b1e2937bb0
tistatos <tistatos@gmail.com>
2020-12-06 12:49:52 +0100

    Day 6 part 2

--- 2020/src/day6.rs
+++ 2020/src/day6.rs
@@ -7,7 +7,7 @@ use std::collections::HashSet;
 pub fn main() {
     let input = get_row_group!("6");
 
-    let group_answers: usize = input
+    let group_any_answers_count: usize = input
         .iter()
         .map(|ans| {
             ans.iter()
@@ -17,5 +17,33 @@ pub fn main() {
                 .len()
         })
         .sum();
-    println!("Day 6 part 1: {:?}", group_answers);
+    let group_all_answers_count: usize = input
+        .iter()
+        .map(|ans| {
+            let mut first = true;
+            ans.iter()
+                .map(|c| c.chars().collect::<HashSet<char>>())
+                .fold(HashSet::new(), |acc, x| {
+                    if first {
+                        first = false;
+                        x
+                    } else {
+                        let intersection = acc.intersection(&x);
+                        let mut hs: HashSet<char> = HashSet::new();
+                        for a in intersection {
+                            hs.insert(*a);
+                        }
+                        hs
+                    }
+                })
+        })
+        .collect::<Vec<HashSet<char>>>()
+        .iter()
+        .map(|h| h.len())
+        .collect::<Vec<usize>>()
+        .iter()
+        .sum();
+
+    println!("Day 6 part 1: {:?}", group_any_answers_count);
+    println!("Day 6 part 2: {:?}", group_all_answers_count);
 }
