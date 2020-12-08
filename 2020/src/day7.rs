2f865eefce02cf225d5e76a87e565bcb6114b6b1
tistatos <tistatos@gmail.com>
2020-12-08 11:18:29 +0100

    Day 7 part 2

--- 2020/src/day7.rs
+++ 2020/src/day7.rs
@@ -4,29 +4,36 @@ use std::collections::HashMap;
 
 use advent_of_code::get_string_rows;
 
-fn contains_shiny_gold(bags: &HashMap<String, Vec<String>>, bag: &String) -> bool {
+fn contains_shiny_gold(bags: &HashMap<String, Vec<(usize, String)>>, bag: &String) -> bool {
     let content = bags.get(bag).unwrap();
-    for b in content {
-        if b == "shiny gold" || contains_shiny_gold(bags, b) {
-            return true;
-        }
-    }
-    false
+    content
+        .iter()
+        .any(|b| b.1 == "shiny gold" || contains_shiny_gold(bags, &b.1))
+}
+
+fn bag_count(bags: &HashMap<String, Vec<(usize, String)>>, bag: &String) -> usize {
+    let content = bags.get(bag).unwrap();
+    content
+        .iter()
+        .map(|b| b.0 + b.0 * bag_count(&bags, &b.1))
+        .sum()
 }
 
 pub fn main() {
     let input = get_row_input!("7");
 
-    let mut bags: HashMap<String, Vec<String>> = HashMap::new();
+    let mut bags: HashMap<String, Vec<(usize, String)>> = HashMap::new();
 
     for r in input {
         let splitted: Vec<&str> = r.split("bag").collect();
         let first_bag = splitted[0].trim().to_string();
+
         bags.insert(first_bag.clone(), vec![]);
 
         for s in splitted.iter().skip(1) {
             match s.find(char::is_numeric) {
                 Some(i) => {
+                    let count = &s[i..i + 1].parse::<usize>().unwrap();
                     let name = format!("{}", &s[i + 2..].trim());
                     match bags.get(&name) {
                         Some(_) => {}
@@ -36,7 +43,7 @@ pub fn main() {
                     };
 
                     match bags.get_mut(&first_bag) {
-                        Some(b) => b.push(name.clone()),
+                        Some(b) => b.push((*count, name)),
                         _ => panic!("couldn't find {}!", first_bag),
                     };
                 }
@@ -47,14 +54,11 @@ pub fn main() {
 
     let result = bags
         .iter()
-        .filter_map(|b| {
-            let res = contains_shiny_gold(&bags, &b.0);
-            if res {
-                Some(b)
-            } else {
-                None
-            }
-        })
+        .filter(|b| contains_shiny_gold(&bags, &b.0))
         .count();
     println!("day 7 part 1: {}", result);
+    println!(
+        "day 7 part 2: {}",
+        bag_count(&bags, &"shiny gold".to_string())
+    );
 }
