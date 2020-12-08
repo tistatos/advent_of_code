a223c00b1233f8dc920a04f629ce5934a294d73b
tistatos <tistatos@gmail.com>
2020-12-08 17:16:12 +0100

    Day 8 part 2

--- 2020/src/day8.rs
+++ 2020/src/day8.rs
@@ -9,7 +9,6 @@ fn parse_command(command: &String, ip: &mut i64, acc: &mut i64) {
     let split: Vec<&str> = command.split(" ").collect();
     let inst = split[0];
     let operand = split[1].parse::<i64>().unwrap();
-    println!("{}: {} {}", ip, inst, operand);
     match inst {
         "nop" => {
             *ip += 1;
@@ -26,18 +25,48 @@ fn parse_command(command: &String, ip: &mut i64, acc: &mut i64) {
 }
 
 pub fn main() {
-    let input = get_row_input!("8");
+    let mut input = get_row_input!("8");
     let mut previous_ip = HashSet::<i64>::new();
     let mut ip = 0;
     let mut acc = 0;
+
     loop {
         parse_command(&input[ip as usize], &mut ip, &mut acc);
-
-        if !previous_ip.contains(&ip) {
-            previous_ip.insert(ip);
-        } else {
+        if !previous_ip.insert(ip) {
             println!("Day 8 part 1: {}", acc);
             break;
+        }
+    }
+
+    let mut orig = String::new();
+    let patch: String = "nop +0".to_string();
+    let mut patch_i = 0;
+
+    loop {
+        ip = 0;
+        acc = 0;
+        previous_ip.clear();
+
+        for i in patch_i..input.len() {
+            let ref r = input[i];
+            if r.find("jmp").is_some() {
+                orig = r.clone();
+                input[i] = patch.clone();
+                patch_i = i;
+                break;
+            }
+        }
+        loop {
+            if ip as usize >= input.len() {
+                println!("Day 8 part 2: {}", acc);
+                return;
+            }
+            parse_command(&input[ip as usize], &mut ip, &mut acc);
+            if !previous_ip.insert(ip) {
+                input[patch_i] = orig.clone();
+                patch_i += 1;
+                break;
+            }
         }
     }
 }
