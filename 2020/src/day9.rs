2e7388e9de9de14b1e4c5b4c6669f045c0be8609
tistatos <tistatos@gmail.com>
2020-12-09 14:20:23 +0100

    day 9 gofled

--- 2020/src/day9.rs
+++ 2020/src/day9.rs
@@ -6,44 +6,37 @@ use advent_of_code::get_rows;
 pub fn main() {
     let input: Vec<usize> = get_row_input!("9", usize);
     let window_size = 25;
-    let number: usize =
-        *input
-            .windows(window_size + 1)
-            .filter(|d| {
-                let target = d[window_size];
-                let c =
-                    d.iter()
-                        .enumerate()
-                        .filter(|(_, a)| **a < target)
-                        .filter(|(i, a)| {
-                            d.iter().enumerate().any(|(j, b)| {
-                                if *i == j {
-                                    false
-                                } else {
-                                    target - *a == *b
-                                }
-                            })
-                        })
-                        .count();
-                c == 0
+    let number: usize = input
+        .windows(window_size + 1)
+        .skip_while(|d| {
+            let target = d[window_size];
+            d.iter().enumerate().any(|(i, a)| {
+                d.iter()
+                    .enumerate()
+                    .any(|(j, b)| if i == j { false } else { *a + *b == target })
             })
-            .map(|r| *r.last().unwrap())
-            .collect::<Vec<usize>>()
-            .first()
-            .unwrap();
+        })
+        .next()
+        .map(|r| *r.last().unwrap())
+        .unwrap();
     println!("Day 9 part 1: {:?}", number);
+
     let index = input.iter().position(|&i| i == number).unwrap();
     let mut window_size = 2;
     loop {
-        let sum: Vec<usize> = input[..index]
+        match input[..index]
             .windows(window_size)
-            .filter(|c| c.iter().sum::<usize>() == number)
-            .map(|c| c.iter().min().unwrap() + c.iter().max().unwrap())
-            .collect();
-        if sum.len() == 1 {
-            println!("Day 9 part 2: {} ", sum.first().unwrap());
-            break;
+            .skip_while(|c| c.iter().sum::<usize>() != number)
+            .next()
+        {
+            Some(v) => {
+                let sum = v.iter().min().unwrap() + v.iter().max().unwrap();
+                println!("Day 9 part 2: {} ", sum);
+                break;
+            }
+            None => {
+                window_size += 1;
+            }
         }
-        window_size += 1;
     }
 }
