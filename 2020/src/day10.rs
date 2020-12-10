eef2ced64a47d4fb9961edf4794c27ac5508a47b
tistatos <tistatos@gmail.com>
2020-12-10 11:54:33 +0100

    day 10 part 2

--- 2020/src/day10.rs
+++ 2020/src/day10.rs
@@ -8,13 +8,34 @@ pub fn main() {
 
     input.sort();
     input.insert(0, 0);
-    println!("{:?}", input);
+    input.push(input.last().unwrap() + 3);
+
     let diffs: Vec<usize> = input.windows(2).map(|j| j[1] - j[0]).collect();
-    let ones: usize = diffs.iter().filter(|d| **d == 1).count();
-    let threes: usize = diffs.iter().filter(|d| **d == 3).count();
+    let (ones, threes): (Vec<usize>, Vec<usize>) = diffs.iter().partition(|d| **d == 1);
+    println!("day 10 part 1: {}", ones.len() * threes.len());
 
-    println!("day 10 ones: {}", ones);
-    println!("day 10 threes: {}", threes + 1);
+    let mut diffs: Vec<usize> = input
+        .windows(3)
+        .filter_map(|j| if j[2] - j[0] <= 3 { Some(j[1]) } else { None })
+        .collect();
+    diffs.push(*input.last().unwrap());
 
-    println!("day 10 part 1: {}", ones * (threes + 1));
+    let mut perm = 0;
+    let res: u64 = diffs
+        .windows(2)
+        .fold(vec![0, 0, 0], |mut acc, x| {
+            if x[1] - x[0] == 1 {
+                perm += 1;
+                acc
+            } else {
+                acc[perm] += 1;
+                perm = 0;
+                acc
+            }
+        })
+        .iter()
+        .zip(vec![2, 4, 7])
+        .map(|(x, f)| (f as u64).pow(*x))
+        .product();
+    println!("day 10 part 2: {}", res);
 }
